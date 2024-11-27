96f021c3147983a3109203c71ae0396bea37fa62
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-28 01:07:12 +0300

    feat: Read the different parts of an error message. Identify common types of errors.

--- lib/a_type_error.py
+++ lib/a_type_error.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 
-wrong_type = 'abc' + 123
+wrong_type = 'abc' + str(123)
+
+print(f"The concatenated result is: {wrong_type}")
--- lib/an_assertion_error.py
+++ lib/an_assertion_error.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 
-assert(1 == 2)
+assert(1 == 1), "The values are not equal!"
+
+print("Assertion passed: The values are equal.")
